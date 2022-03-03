8dfad9cad62d7aba76a0362dcdd672ed6e43a368
kokomi simp :3 <beol0127@gmail.com>
2022-03-03 23:31:51 +0000

    chore: creating popup window

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <main>
-      hello
+      idle miner - the idle game
+      {/*
+https://stackoverflow.com/questions/44712076/how-do-i-create-a-popup-window-in-react-js-when-the-button-and-function-are-in-s
+*/}
     </main>
   );
 }
