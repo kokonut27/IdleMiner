cb1215967472c0c09d1b4a73f82ea922f15a9e33
kokomi simp :3 <beol0127@gmail.com>
2022-03-04 18:21:31 +0000

    chore: setup base modal + update syntax

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './idle-miner'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './idle-miner';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('idle-miner')
-)
+);
